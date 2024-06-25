7c9ca34fcf24c217575a93caf7b5470ae199bbfd
benoit-bremaud <benoit.bremaud@laplateforme.io>
2024-06-25 10:27:01 +0200

    feat(message): add Message entity with content, createdAt, author, and recipient properties

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -21,6 +23,22 @@ class User
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
+
+    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'author')]
+    private Collection $recipient;
+
+    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'author')]
+    private Collection $sentMessages;
+
+    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'recipient')]
+    private Collection $receiveMessages;
+
+    public function __construct()
+    {
+        $this->recipient = new ArrayCollection();
+        $this->sentMessages = new ArrayCollection();
+        $this->receiveMessages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +77,96 @@ class User
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getRecipient(): Collection
+    {
+        return $this->recipient;
+    }
+
+    public function addRecipient(Message $recipient): static
+    {
+        if (!$this->recipient->contains($recipient)) {
+            $this->recipient->add($recipient);
+            $recipient->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipient(Message $recipient): static
+    {
+        if ($this->recipient->removeElement($recipient)) {
+            // set the owning side to null (unless already changed)
+            if ($recipient->getAuthor() === $this) {
+                $recipient->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getSentMessages(): Collection
+    {
+        return $this->sentMessages;
+    }
+
+    public function addSentMessage(Message $sentMessage): static
+    {
+        if (!$this->sentMessages->contains($sentMessage)) {
+            $this->sentMessages->add($sentMessage);
+            $sentMessage->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSentMessage(Message $sentMessage): static
+    {
+        if ($this->sentMessages->removeElement($sentMessage)) {
+            // set the owning side to null (unless already changed)
+            if ($sentMessage->getAuthor() === $this) {
+                $sentMessage->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getReceiveMessages(): Collection
+    {
+        return $this->receiveMessages;
+    }
+
+    public function addReceiveMessage(Message $receiveMessage): static
+    {
+        if (!$this->receiveMessages->contains($receiveMessage)) {
+            $this->receiveMessages->add($receiveMessage);
+            $receiveMessage->setRecipient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReceiveMessage(Message $receiveMessage): static
+    {
+        if ($this->receiveMessages->removeElement($receiveMessage)) {
+            // set the owning side to null (unless already changed)
+            if ($receiveMessage->getRecipient() === $this) {
+                $receiveMessage->setRecipient(null);
+            }
+        }
 
         return $this;
     }
